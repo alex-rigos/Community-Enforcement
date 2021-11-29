8079224a54125c7882cfeab05eb418a3d5877d61
Alex Rigos <alexandros.rigos@gmail.com>
2021-11-29 16:44:37 +0100

    Automate arrow plot generation

--- ComEn-Arrows.jl
+++ ComEn-Arrows.jl
@@ -9,44 +9,70 @@ end
 
 # Distribute definitions to worker threads
 @everywhere include("ComEn-Definitions.jl") # Definitions of our types and methods
-@everywhere include("ComEn-Parameters.jl") # Parameters for the simulation
 
-# How many times to sample for each point?
-@everywhere N = 500
+for model in ["Analytical","Baseline"]
+    @everywhere model = $model
+# model = "Analytical"
+# model = "Baseline"
 
-#  Check that parameters do not give negative payoffs
-warning()
+    @everywhere include($(string("ComEn-Parameters",model,".jl"))) # Parameters for the simulation
+    println("$model")
+    println("v=$v")
+    for l = 2.:1.:4
+        for f = .25:.25:.75
 
-np = 160  # number of producers
-prodstep = np/20  # step increment (grid) for producers
-ne = 40  # number of enforcers
-enfstep = ne/20  # step increment (grid) for enforcers
+            @everywhere global f = $(f)
+            @everywhere global l = $(l)
 
-pops = []  # Vector to hold populations
-for CP = prodstep:prodstep:np-prodstep
-    for CE = enfstep:enfstep:ne-enfstep
-# for CP = 4:4:16
-#          for CE = 1:1:4
-        pop=[
-            ["cooperator", CP],
-            ["defector", np - CP],
-            ["peacekeeper", CE],
-            ["shortsighted", ne - CE],
-            ]
-            push!(pops,pop)
+            # How many times to sample for each point?
+            @everywhere N = 5
+
+            #  Check that parameters do not give negative payoffs
+            warning()
+
+            np = 160  # number of producers
+            prodstep = np/20  # step increment (grid) for producers
+            ne = 40  # number of enforcers
+            enfstep = ne/20  # step increment (grid) for enforcers
+
+            pops = []  # Vector to hold populations
+            for CP = prodstep:prodstep:np-prodstep
+                for CE = enfstep:enfstep:ne-enfstep
+            # ATTENTION: Order matters here for where they are being plotted. Use exactly 4 srategies.
+                    pop=[
+                        ["CP", CP],  # x-axis x=1
+                        ["DP", np - CP],  # x-axis x=0
+                        ["CE", CE],  # y-axis y=1
+                        ["DE", ne - CE],  # y-axis y=0
+                        ]
+                        push!(pops,pop)
+                end
+            end
+            @everywhere agentStrategyNames = map(x->x[1],$pops[1])
+            @everywhere agentStrategies = map(x->stratByName(x[1],allStrategies),$pops[1])
+
+            results = pmap(calcArrow,pops)
+
+            xx,yy,uu,zz = map(x->getindex.(results,x),1:4); 
+
+
+            # Scale down the arrows (adjust so that they don't overlap)
+            scale = 15
+            paramString = "l=$(l) f=$(f)"
+            titleString = string(model,": ",paramString)
+            # plotly()
+            gr()
+            quiver(xx, yy, quiver=(uu/scale,zz/scale),xlims=[0,1],ylims=[0,1], 
+            # xlabel="$(agentStrategyNames[1]) vs. $(agentStrategyNames[2])",
+            # ylabel="$(agentStrategyNames[3]) vs. $(agentStrategyNames[4])",
+            clip_on=true, thickness_scaling=1.5, aspect_ratio=:equal)
+            annotate!(0,-0.12,text("$(agentStrategyNames[2])",:center,10))
+            annotate!(1,-0.12,text("$(agentStrategyNames[1])",:center,10))
+            annotate!(-.15,0,text("$(agentStrategyNames[4])",:right,10))
+            annotate!(-.15,1,text("$(agentStrategyNames[3])",:right,10))
+            title!(titleString)
+            plot!(size=(600,600))
+            savefig("./Figs/$(model)-model/arrow-plots/$(paramString).pdf")
+        end
     end
 end
-@everywhere agentStrategyNames = map(x->x[1],$pops[1])
-@everywhere agentStrategies = map(x->stratByName(x[1],allStrategies),$pops[1])
-
-results = pmap(calcArrow,pops)
-
-xx,yy,uu,zz = map(x->getindex.(results,x),1:4);
-
-##
-# Scale down the arrows (adjust so that they don't overlap)
-scale = 20
-
-quiver(xx, yy, quiver=(uu/scale,zz/scale),xlims=[0,1],ylims=[0,1], 
-xlabel="$(agentStrategyNames[1]) vs. $(agentStrategyNames[2])",
-ylabel="$(agentStrategyNames[3]) vs. $(agentStrategyNames[4])")

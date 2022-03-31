f64d525b2fe65d2015836b3a69909b13ad6a7145
Alex Rigos <alexandros.rigos@gmail.com>
2022-03-31 18:32:43 +0200

    Clean up comp-stat

--- comp-stat-data-generation.jl
+++ comp-stat-data-generation.jl
@@ -6,37 +6,56 @@ num_procs = 8
 if nworkers() < min(length(Sys.cpu_info()),num_procs)
     addprocs(min(length(Sys.cpu_info()),num_procs) - nworkers())
 end
+
 @everywhere include("ComEn-Definitions.jl") # Definitions of our types and methods
+@everywhere model = "Baseline" # Choose which file parameters are read from. They are read from "ComEn-Parameters$(model)"
 
-@everywhere generations = 100000
-@everywhere model = "Baseline"
+# @everywhere generations = 100000
+@everywhere generations = 10
 
-ranges = [
-    ["f",.2:.1:.6,"fixed cost of punishment"],["v",.05:.05:.25,"variable cost of punishment"],["l",2.:.5:4,"loss from fight"],
-["b",3.:.5:5.,"benefit from cooperation"],["c",.5:.5:2.5,"cost of cooperation"],["w",1.:.5:3.,"background payoff"],
-["p",.5:.5:2.5,"loss from being punished"],["τ",.1:.2:.9,"tax rate"],["ψ",.1:.2:.9,"capture from win"],
-["δ",.1:.2:.9,"Continuation probability"],["κ",1:5,"Number of punishment periods"],["μ",.01:.01:.05,"Probability of action mistakes"],
-["η",[.01,.1,.15,.2,.25],"Logit noise"],["ε",0.05:0.05:.25,"Probability of revision mistake"],
-["revisionVector",[
-    [[Agent,.1],
-    [Enforcer,0.3],
-    [Producer,.6]],
-    [[Agent,.33],
-    [Enforcer,0.33],
-    [Producer,.34]],
-    [[Agent,1.],
-    [Enforcer,0.],
-    [Producer,0.]]
-],"Probability of global revision"]
+rootdir = "comp-stat/"  # Directory where data is stored
+
+ranges = [  # Parameters and parameter value ranges for which comparative statics should be calculated. The third entry is the label of the x-axis
+    ["f",.2:.1:.6,"fixed cost of punishment"],
+    ["v",.05:.05:.25,"variable cost of punishment"],
+    ["l",2.:.5:4,"loss from fight"],
+    ["b",3.:.5:5.,"benefit from cooperation"],
+    ["c",.5:.5:2.5,"cost of cooperation"],
+    ["w",1.:.5:3.,"background payoff"],
+    ["p",.5:.5:2.5,"loss from being punished"],
+    ["τ",.1:.2:.9,"tax rate"],
+    ["ψ",.1:.2:.9,"capture from win"],
+    ["δ",.1:.2:.9,"Continuation probability"],
+    ["κ",1:5,"Number of punishment periods"],
+    ["μ",.01:.01:.05,"Probability of action mistakes"],
+    ["η",[.01,.1,.15,.2,.25],"Logit noise"],
+    ["ε",0.05:0.05:.25,"Probability of revision mistake"],
+    ["revisionVector",
+        [
+            [
+                [Agent,.1],
+                [Enforcer,0.3],
+                [Producer,.6]
+            ],
+            [
+                [Agent,.33],
+                [Enforcer,0.33],
+                [Producer,.34]
+            ],
+            [
+                [Agent,1.],
+                [Enforcer,0.],
+                [Producer,0.]
+            ]
+        ],
+        "Probability of global revision"]
 ]
 
 data = []
 
 for ran in ranges  # Loop for the different parameters
-    
     parameter = ran[1]
     values = ran[2]
-
     pops = [       # Vector of initial conditions
         [["CP",10],
         ["CE",2],
@@ -64,11 +83,11 @@ for ran in ranges  # Loop for the different parameters
 
     for val in values
         @everywhere setParameters($(parameter),$(val))
-        dir = "line-plots/$(parameter)/"
+        dir = "$(rootdir)comp-stat-data/$(parameter)/"
         mkpath(dir)
         dat = pmap(x->timeAverage(x,generations),pops)
-        # append!(mat,dat)
-        CSV.write("$(dir)$(parameter)=$(val).csv",Tables.table(mapreduce(permutedims, vcat, dat)))
-        append!(data, [[[parameter,val],dat]])
+        file = "$(dir)$(parameter)=$(val).csv"
+        CSV.write(file,Tables.table(mapreduce(permutedims, vcat, dat)))
+        println(file)
     end
 end
--- comp-stat-plots.jl
+++ comp-stat-plots.jl
@@ -4,34 +4,32 @@ using CSV, Tables, LaTeXStrings,ColorSchemes
 include("ComEn-Definitions.jl")
 include("ColorDefinitions.jl")
 ranges = [
-    ["f",.2:.1:.6,"fixed cost of punishment",true,:bottomleft,"(a)"],
-    ["v",.05:.05:.25,"variable cost of punishment",true,:bottom,"(b)"],
-    ["l",2.:.5:4,"loss from fight",true,:bottomleft,"(c)"],
-["b",3.:.5:5.,"benefit from cooperation",true,:bottomleft,"(d)"],
-["c",.5:.5:2.5,"cost of cooperation",true,:bottom,"(e)"],
-["w",1.:.5:3.,"background payoff",true,:bottomleft,"(f)"],
-["p",.5:.5:2.5,"loss from being punished",true,:bottom,"(g)"],
-["τ",.1:.2:.9,"tax rate",true,:bottom,"(h)"],
-["ψ",.1:.2:.9,"capture from win",true,:bottomleft,"(i)"],
-["δ",.1:.2:.9,"continuation probability",true,:bottomright,"(a)"],
-["κ",1:5,"number of punishment periods",true,:bottomright,"(b)"],
-["μ",.01:.01:.05,"probability of action mistakes",true,:bottomright,"(c)"],
-["η",[.01,.1,.15,.2,.25],"logit noise",true,:bottomright,"(d)"],
-["ε",0.05:0.05:.25,"probability of revision mistake",true,:bottomright,"(e)"],
-["revisionVector",[
-    [[Agent,.1],
-    [Enforcer,0.3],
-    [Producer,.6]],
-    [[Agent,.33],
-    [Enforcer,0.33],
-    [Producer,.34]],
-    [[Agent,1.],
-    [Enforcer,0.],
-    [Producer,0.]]
-],"probability of global revision",true,:bottomright,"(f)"]
+    ["f",.2:.1:.6,"fixed cost of punishment",true,:bottomleft],
+    ["v",.05:.05:.25,"variable cost of punishment",true,:bottom],
+    ["l",2.:.5:4,"loss from fight",true,:bottomleft],
+    ["b",3.:.5:5.,"benefit from cooperation",true,:bottomleft],
+    ["c",.5:.5:2.5,"cost of cooperation",true,:bottom],
+    ["w",1.:.5:3.,"background payoff",true,:bottomleft],
+    ["p",.5:.5:2.5,"loss from being punished",true,:bottom],
+    ["τ",.1:.2:.9,"tax rate",true,:bottom],
+    ["ψ",.1:.2:.9,"capture from win",true,:bottomleft],
+    ["δ",.1:.2:.9,"continuation probability",true,:bottomright],
+    ["κ",1:5,"number of punishment periods",true,:bottomright],
+    ["μ",.01:.01:.05,"probability of action mistakes",true,:bottomright],
+    ["η",[.01,.1,.15,.2,.25],"logit noise",true,:bottomright],
+    ["ε",0.05:0.05:.25,"probability of revision mistake",true,:bottomright],
+    ["revisionVector",[
+        [[Agent,.1],
+        [Enforcer,0.3],
+        [Producer,.6]],
+        [[Agent,.33],
+        [Enforcer,0.33],
+        [Producer,.34]],
+        [[Agent,1.],
+        [Enforcer,0.],
+        [Producer,0.]]
+    ],"probability of global revision",true,:bottomright]
 ]
-
-
 
 for ran in ranges
     parameter = ran[1]
@@ -39,68 +37,44 @@ for ran in ranges
     title = ran[3]
     show_baseline = ran[4]
     legend_place = ran[5]
-    subfig_label = ran[6]
 
-# parameter = "f"
-# range = .2:.1:.6
-# title = ranges[1][3]
-        xCP = []
-        xCE = []
-        xP = []
-        sCP = []
-        sCE = []
-        sP = []
-        dir = "line-plots/$(parameter)"
-        y = []
+    rootdir = "comp-stat/"
+    readdir = "$(rootdir)comp-stat-data/$(parameter)/"
+    writedir = "$(rootdir)comp-stat-figures/"
+
+    xCP = []
+    xCE = []
+    xP = []
+    sCP = []
+    sCE = []
+    sP = []
+    y = [] # Where the data to be plotted is stored
+    
     for val in rang
-        df = CSV.File("$(dir)/$(parameter)=$(val).csv")|> Tables.matrix
+        df = CSV.File("$(readdir)$(parameter)=$(val).csv")|> Tables.matrix
         len =length(df[:,1])
-        append!(xCP, mean(map(i->df[i,1]/(df[i,1]+df[i,3]),1:len)))
-        append!(xCE, mean(map(i->df[i,2]/(df[i,2]+df[i,4]),1:len)))
-        append!(xP, mean(map(i->(df[i,1]+df[i,3])/(sum(df[i,:])),1:len)))
-        
-        # For standard deviations
-        append!(sCP, Statistics.std(map(i->df[i,1]/(df[i,1]+df[i,3])/sqrt(len),1:len)))
-        append!(sCE, Statistics.std(map(i->df[i,2]/(df[i,2]+df[i,4])/sqrt(len),1:len)))
-        append!(sP, Statistics.std(map(i->(df[i,1]+df[i,3])/(sum(df[i,:]))/sqrt(len),1:len)))
-        # println("xCP=$(xCP)")
-        # println("sCP=$(sCP)")
-        # for i in 1:length(df[:,1])
-        #     xCP[i] = df[i,1]/(df[i,1]+df[i,3])
-        #     xCE[i] = df[i,2]/(df[i,2]+df[i,4])
-        #     xP[i] = (df[i,1]+df[i,3])/(sum(df[i,:]))
-        # end
+        append!(xCP, mean(map(i->df[i,1]/(df[i,1]+df[i,3]),1:len))) # Proportion of CP among producers
+        append!(xCE, mean(map(i->df[i,2]/(df[i,2]+df[i,4]),1:len))) # Proportion of CE among enforcers
+        append!(xP, mean(map(i->(df[i,1]+df[i,3])/(sum(df[i,:])),1:len))) # Proportion of producers among agents
+
         append!(y,[xCP,xCE,xP,sCP,sCE,sP])
     end
    
     pgfplotsx()
-    # gr()
-    # pyplot(markershape = :auto)
-    # pyplot()
-    # names = [L"\frac{n_{CP}}{n_P}", L"\frac{n_{CE}}{n_E}", L"\frac{n_P}{n}"]
     names = [L"n_{CP}/n_P"  L"n_{CE}/n_E"  L"n_P/N"]
-    # pointnames = [L"n_{CP}/n_P", L"n_{CE}/n_E", L"n_P/n", ""]
-    # linenames = ["","","", "baseline"]
-    # styles = [:solid, :dash, :dashdot]
     styles = [:solid, :solid, :solid]
     plot(layout=(1,1),tickfontfamily="Helvetica",clip_on=true)
     if parameter == "revisionVector"
         rang = map(i->rang[i][1][2],1:length(rang))
     end
 
-    # for i in 1:length(names)
-        # c = ColorSchemes.Paired_10[((2*i)./10)]
-        # c = ColorSchemes.Paired_10[((2*i-1)./10)]
-        # c = get(ColorSchemes.hawaii,i./length(names))
-        # c = get(ColorSchemes.rainbow,i./length(names))
-        sh = [:circle  :rect  :utriangle  :nothing]
-        ms = [4 4 6]
-        # plot!(rang,y[i],label=names[i],yerror=y[i+3],markershape = sh,markersize=ms,legend = legend_place,linestyle = styles[i],color = c,lw = 1.5, ylims = (0, 1),fontfamily = "Helvetica",linealpha=1,seriestype=:line)
-        plot!(rang,y[1:3],label=names,markershape = sh,markersize=ms,
-        legend = legend_place,linestyle = styles,color = mycompstatcolors,
-        lw = 1.5, ylims = (0, 1),linealpha=1,seriestype=:line,
-        tickfontfamily="Helvetica",guidefontsize=15,tickfontsize=12,titlefont="Courier",legendfontsize=12)
-    # end
+    sh = [:circle  :rect  :utriangle  :nothing]
+    ms = [4 4 6]
+    plot!(rang,y[1:3],label=names,markershape = sh,markersize=ms,
+    legend = legend_place,linestyle = styles,color = mycompstatcolors,
+    lw = 1.5, ylims = (0, 1),linealpha=1,seriestype=:line,
+    tickfontfamily="Helvetica",guidefontsize=15,tickfontsize=12,titlefont="Courier",legendfontsize=12)
+    
     include("ComEn-ParametersBaseline.jl")
     
     s = Symbol(parameter)
@@ -111,17 +85,15 @@ for ran in ranges
     if show_baseline
         plot!([theval],label=L"\textrm{\sffamily baseline}",seriestype=:vline,markersize=0,lw=1,style = :dash,color = "black")
     end
-    # plot(range,[xCP,xCE,xP],linestyle=[:solid :dash :dashdot],linecolor = ["black" "blue" "red"],lw = 2, ylims = (0, 1))
     parametertx = parameter # LaTeX-friendly text to use for the parameter name
     greeknames = Dict("ψ" => "\\psi", "τ"=>"\\tau","δ" => "\\delta","κ" => "\\kappa","μ" => "\\mu","η" => "\\eta","ε" => "\\varepsilon","revisionVector" => "\\gamma_{P,E}")
     if parameter in keys(greeknames)
         parametertx = greeknames[parameter]
     end
-    plot!(title=subfig_label,titlelocation=:left,titlefont=font("Helvetica"))
     plot!(xlabel=L"\textrm{\sffamily %$title}~%$parametertx",fontfamily = "Helvetica")
     plot!(size=(440,330))
-# plot!(ylabel=L"\textrm{\sffamily Probability of~}n\leq n_{XL\cap DCA}", fontfamily="Computer Modern") 
-    savefig("comp-stat/comp-stat-$(parameter).pdf")
-
-
+    mkpath(writedir)
+    file = "$(writedir)comp-stat-$(parameter).pdf"
+    savefig(file)
+    println(file)   
 end

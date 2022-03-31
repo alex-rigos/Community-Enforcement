95971c2b50b3ac03214db4492dacd705a41b9558
Alex Rigos <alexandros.rigos@gmail.com>
2022-03-31 22:40:50 +0200

    Add master script for paper figures

---
+++ paper-figures-replication.jl
@@ -0,0 +1,4 @@
+include("paper-figures/paper-time-series-plots.jl")
+include("paper-figures/paper-time-averages-plots.jl")
+include("paper-figures/paper-fishtank-plots.jl")
+include("paper-figures/paper-comp-stat-plots.jl")
